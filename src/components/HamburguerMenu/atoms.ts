49adc5168e5189df945ff8f4a76a25aabb0273d0
ArielBetti <ariel.betti@gmail.com>
2022-08-23 18:13:25 -0300

    fix(hamburguer-menu): fix min-height drawer

--- src/components/HamburguerMenu/atoms.ts
+++ src/components/HamburguerMenu/atoms.ts
@@ -24,7 +24,7 @@ export const HamburguerMenuCollapse = styled.div`
   position: absolute;
   top: 100%;
   right: 0;
-  height: 100%;
+  min-height: 100vh;
   background-color: ${({ theme }: { theme: IWebettiTheme }) =>
     theme?.colors?.neutral?.pure};
   min-width: ${({ isOpen }: IWebettiHamburguerMenuCollapse) =>
